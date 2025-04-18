25e44bdef7b1a92e81ce9afcb3378f91edd32f75
Daniel <ury14@txstate.edu>
2025-04-18 02:15:34 -0500

    Made Deck Stack Global

--- main.c
+++ main.c
@@ -6,17 +6,19 @@
 
 int INT_CHIPS_IN_BAG = 50;
 int INT_BAGS_EATEN = 0;
+int END_GAME = 0; //0 = false, 1 = true (end game)
 
 //checking if I pushed to working-branch
 
-//suits = 4
-//0=spades, 1=heart, 2=clubs, 3=diamonds
-//ranks = 13
-// a=1,2,3,4,5,6,7,8,9,10, j=11, q=12,k=13
+enum Suit
+{
+    SPADES, HEARTS, CLUBS, DIAMONDS
+};
+
 struct Card
 {
-    unsigned int suit;
-    unsigned int rank;
+    enum Suit suit;
+    size_t rank;
 };
 
 //generic linked list
@@ -25,6 +27,25 @@ struct Node
     void *data;
     struct Node *next;
 };
+
+struct Hand
+{
+    struct Card card;
+    int bool_is_empty;
+};
+
+struct Player
+{
+    struct Hand left_hand;
+    struct Hand right_hand;
+    int int_chips_to_eat;
+    int int_player_number;
+    int bool_is_dealer;
+};
+
+//global declarations
+struct Card greasy_card;
+struct Node *stack_deck = NULL;                 //much easier to deal with a deck as a linked list/stack
 
 //insert element into list
 void push(struct Node **top, void *data, size_t data_size)
@@ -79,7 +100,7 @@ void intialize_deck(struct Card deck[], int int_max_suit, int int_max_rank)
     {
         for(int j = 1; j < int_max_rank+1; j++)
         {
-            deck[int_card_count].suit = i;
+            deck[int_card_count].suit = (enum Suit) i;
             deck[int_card_count].rank = j;
             int_card_count++;
         }
@@ -106,43 +127,65 @@ void shuffle_deck(struct Card deck[], int int_deck_size)
 }
 
 //uses stack functions to 
-void stack_card_deck(struct Node **stack_deck, struct Card deck[], int int_max_deck_size)
+void stack_card_deck(struct Card deck[], int int_max_deck_size)
 {
     for(int i = 0; i < int_max_deck_size; i++)
     {
-        push(stack_deck, &deck[i], sizeof(struct Card));
+        push(&stack_deck, &deck[i], sizeof(struct Card));
     }
 }
 
-void print_stack_card_deck(struct Node **stack_deck)
+void print_stack_card_deck()
 {
     struct Card card_current;
        
-    while(*stack_deck != NULL)
+    while(&stack_deck != NULL)
     {
-        top(*stack_deck, &card_current, sizeof(struct Card));
-        printf("%u of %u\n", card_current.suit, card_current.rank);
-        pop(stack_deck);
+        top(stack_deck, &card_current, sizeof(struct Card));
+        printf("%d of %zu\n", card_current.suit, card_current.rank);
+        pop(&stack_deck);
+    }
+}
+
+void declare_greasy_card()
+{
+    top(stack_deck, &greasy_card, sizeof(struct Card));
+    pop(&stack_deck);
+    printf("The Greasy card is %d of %zu", greasy_card.suit, greasy_card.rank);
+}
+
+void draw_card(struct Player player)
+{
+    if(player.left_hand.bool_is_empty == 0)
+    {
+        //top
     }
 }
 
 //n players, m chips per bag, o seed for randomizer
 int main()
 {
+    int players = 1;
     int int_max_deck_size = 4 * 13;
     struct Card card_deck[int_max_deck_size];       //much easier to shuffle while an array
-    struct Node *stack_deck = NULL;                 //much easier to deal with a deck as a linked list/stack
+    
 
     //seed's randomizer, NULL for the mean time
     srand(time(NULL));
 
     intialize_deck(card_deck, 4, 13);
     shuffle_deck(card_deck, int_max_deck_size);
+    stack_card_deck(card_deck, int_max_deck_size);
+    declare_greasy_card();
+    
+    while(END_GAME)
+    {
 
-    //printf("here in main\n");
-
-    stack_card_deck(&stack_deck, card_deck, int_max_deck_size);
-    print_stack_card_deck(&stack_deck);
+    }
+    
+    
+    
+    //print_stack_card_deck(&stack_deck);
 
     return 0;
 }
