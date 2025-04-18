25325aebe85a51f50f79726e7c5e6e4846119ddb
Daniel <ury14@txstate.edu>
2025-04-18 04:22:43 -0500

    Added basic game functionality

--- main.c
+++ main.c
@@ -4,15 +4,21 @@
 #include <stdlib.h>     //rand()
 #include <time.h>       //seeding for rand()
 
+
+//bool macros so I dont lose my mind
+#define true 1
+#define false 0
+
 int INT_CHIPS_IN_BAG = 50;
 int INT_BAGS_EATEN = 0;
-int END_GAME = 0; //0 = false, 1 = true (end game)
+int END_GAME = false; 
 
 //checking if I pushed to working-branch
 
 enum Suit
 {
-    SPADES, HEARTS, CLUBS, DIAMONDS
+    //0   1       2       3      4
+    NONE, SPADES, HEARTS, CLUBS, DIAMONDS
 };
 
 struct Card
@@ -45,7 +51,8 @@ struct Player
 
 //global declarations
 struct Card greasy_card;
-struct Node *stack_deck = NULL;                 //much easier to deal with a deck as a linked list/stack
+struct Node *stack_deck = NULL;     //much easier to deal with a deck as a linked list/stack
+struct Card card_deck[52];            //much easier to shuffle while an array
 
 //insert element into list
 void push(struct Node **top, void *data, size_t data_size)
@@ -96,7 +103,7 @@ void top(struct Node *top, void *data_out, size_t data_size)
 void intialize_deck(struct Card deck[], int int_max_suit, int int_max_rank)
 {
     int int_card_count = 0;
-    for(int i = 0; i < int_max_suit; i++)
+    for(int i = 1; i < int_max_suit+1; i++)
     {
         for(int j = 1; j < int_max_rank+1; j++)
         {
@@ -151,32 +158,35 @@ void declare_greasy_card()
 {
     top(stack_deck, &greasy_card, sizeof(struct Card));
     pop(&stack_deck);
-    printf("The Greasy card is %d of %zu", greasy_card.suit, greasy_card.rank);
+    printf("The Greasy card is %d of %zu\n", greasy_card.suit, greasy_card.rank);
 }
 
-void draw_card(struct Player player)
+void draw_card(struct Player *player)
 {
     //choose a hand, left or right hand
     //give that hand a card
     int int_random_hand = rand() % 2; // 0 = left, 1 = right
+    printf("The player has drawn a card... ");
 
     //random declares left
     if(int_random_hand == 0)
     {
         //if left is empty, give card
-        if(player.hand_left.bool_is_empty)
+        if(player->hand_left.bool_is_empty)
         {
-            top(stack_deck, &player.hand_left, sizeof(struct Card));
+            top(stack_deck, &player->hand_left.card, sizeof(struct Card));
             pop(&stack_deck);
-            player.hand_left.bool_is_empty = 0;
+            player->hand_left.bool_is_empty = false;
+            printf("and they chose to place their card in their left hand, they placed the %d of %zu...\n", player->hand_left.card.suit, player->hand_left.card.rank);
         }
 
         //otherwise, that means right is empty, so we give them the card
         else
         {
-            top(stack_deck, &player.hand_right, sizeof(struct Card));
+            top(stack_deck, &player->hand_right.card, sizeof(struct Card));
             pop(&stack_deck);
-            player.hand_right.bool_is_empty = 0;
+            player->hand_right.bool_is_empty = false;
+            printf("and wanted to place their card in their left hand, but it was full, so they placed that card in their right hand, they placed the %d of %zu\n", player->hand_right.card.suit, player->hand_right.card.rank);
         }
     }
 
@@ -184,52 +194,88 @@ void draw_card(struct Player player)
     else if(int_random_hand == 1)
     {
         //if right is empty, give card
-        if(player.hand_right.bool_is_empty)
+        if(player->hand_right.bool_is_empty)
         {
-            top(stack_deck, &player.hand_right, sizeof(struct Card));
+            top(stack_deck, &player->hand_right.card, sizeof(struct Card));
             pop(&stack_deck);
-            player.hand_right.bool_is_empty = 0;
+            player->hand_right.bool_is_empty = false;
+            printf("and they chose to place their card in their right hand, they placed the %d of %zu...\n", player->hand_right.card.suit, player->hand_right.card.rank);
         }
 
         //otherwise, that means right is empty, so we give them the card
         else
         {
-            top(stack_deck, &player.hand_left, sizeof(struct Card));
+            top(stack_deck, &player->hand_left.card, sizeof(struct Card));
             pop(&stack_deck);
-            player.hand_left.bool_is_empty = 0;
+            player->hand_left.bool_is_empty = false;
+            printf("and wanted to place their card in their right hand, but it was full, so they placed that card in their left hand, they placed the %d of %zu\n", player->hand_left.card.suit, player->hand_left.card.rank);            
         }
     }
 }
 
-void discard_card(struct Player player)
+void discard_card(struct Player *player)
 {
     //choose a hand, left or right hand
     //discard that card, from hand
     int int_random_hand = rand() % 2; // 0 = left, 1 = right
 
+    struct Card temp = {SPADES, 0};
+
     //discard the left hand
-    if(int_random_hand == 0 && !player.hand_left.bool_is_empty)
+    if(int_random_hand == 0 && !player->hand_left.bool_is_empty)
     {
         //place the card at the bottom of the deck
         //empty the player's left hand
+        printf("The Player chose their left hand and discarded the %d of %zu\n", player->hand_left.card.suit, player->hand_left.card.rank);
+        player->hand_left.bool_is_empty = true;
+        player->hand_left.card = temp;
     }
 
-    if(int_random_hand && !player.hand_right.bool_is_empty)
+    if(int_random_hand && !player->hand_right.bool_is_empty)
+    {
+        //place the card at the bottom of the deck
+        //empty the player's right hand
+        printf("The Player chose their right hand and discarded the %d of %zu\n", player->hand_right.card.suit, player->hand_right.card.rank);
+        player->hand_right.bool_is_empty = true;
+        player->hand_right.card = temp;
+    }
+
+    
+}
+
+void compare_cards(struct Player *player)
+{
+    printf("The Player is looking at their hand\n");
+    //check left hand matches rank with greasy card
+    if(player->hand_left.card.rank == greasy_card.rank)
+    {
+        END_GAME = true;
+        printf("The match card was the player's %d of %zu and the greasy card was %d of %zu\n", player->hand_left.card.suit, player->hand_left.card.rank, greasy_card.suit, greasy_card.rank);
+        return;
+    }
+
+    //check right hand mataches rank with greasy card
+    if(player->hand_right.card.rank == greasy_card.rank)
+    {
+        END_GAME = true;
+        printf("The match card was the player's %d of %zu and the greasy card was %d of %zu\n", player->hand_right.card.suit, player->hand_right.card.rank, greasy_card.suit, greasy_card.rank);
+        return;
+    }
+
+    printf("The player has checked, there was no matches\n");
 }
 
 //n players, m chips per bag, o seed for randomizer
 int main()
 {
     struct Player players[1];
-    players[0].hand_left.bool_is_empty = 1;
-    players[0].hand_right.bool_is_empty = 1;
+
+    //player intialization
+    players[0].hand_left.bool_is_empty = true;
+    players[0].hand_right.bool_is_empty = true;
     players[0].bool_is_dealer = 1;
 
-
     int int_max_deck_size = 4 * 13;
-    struct Card card_deck[int_max_deck_size];       //much easier to shuffle while an array
-    
-    
 
     //seed's randomizer, NULL for the mean time
     srand(time(NULL));
@@ -239,9 +285,15 @@ int main()
     stack_card_deck(card_deck, int_max_deck_size);
     declare_greasy_card();
     
-    while(END_GAME)
+    //dealt a card
+    draw_card(&players[0]);
+    
+
+    while(!END_GAME)
     {
-        draw_card()
+        draw_card(&players[0]);
+        compare_cards(&players[0]);
+        discard_card(&players[0]);
     }
     
     
